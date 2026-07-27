740e08d1d1ce02550ed930bd2a474d70caac7de6
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-07-27 22:06:07 +0700

    Refactor Report and Dashboard controller

---
+++ src/06.WebApi/Controllers/ReportsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TicketManagement.Application.Interfaces;
+using TicketManagement.Shared.Constants;
+using TicketManagement.Shared.Dtos.Reports;
+
+namespace TicketManagement.WebApi.Controllers;
+
+[ApiController]
+[Route(ApiRoutes.Reports.Base)]
+[Authorize(Roles = RoleConstants.Manager)]
+public class ReportsController(IReportService reportService) : ControllerBase
+{
+
+    [HttpGet(ApiRoutes.Reports.ManagerReport)]
+    public async Task<IActionResult> GetManagerReport(
+    [FromQuery] ManagerReportFilterDto filter)
+    {
+        var result = await reportService.GetManagerReportAsync(filter);
+        return Ok(result);
+    }
+}
