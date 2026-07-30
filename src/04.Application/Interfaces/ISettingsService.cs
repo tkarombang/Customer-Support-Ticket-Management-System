d4c3a3466c1e335ddea6e11daf57a6680a7bf6fb
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-07-30 21:07:29 +0700

    V2-Add Service and Interface for settings

---
+++ src/04.Application/Interfaces/ISettingsService.cs
@@ -0,0 +1,13 @@
+﻿using TicketManagement.Shared.Dtos.Settings;
+
+namespace TicketManagement.Application.Interfaces
+{
+    public interface ISettingsService
+    {
+        Task<GeneralSettingDto> GetGeneralAsync();
+        Task UpdateGeneralAsync(GeneralSettingDto dto, Guid updatedBy);
+
+        Task<SlaSettingDto> GetSlaAsync();
+        Task UpdateSlaAsync(SlaSettingDto dto, Guid updatedBy);
+    }
+}
---
+++ src/04.Application/Services/SettingsService.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using TicketManagement.Application.Interfaces;
+using TicketManagement.Domain.Entities;
+using TicketManagement.Domain.Interfaces;
+using TicketManagement.Shared.Dtos.Settings;
+
+namespace TicketManagement.Application.Services;
+
+public class SettingsService(IAppSettingRepository appSettingRepository) : ISettingsService
+{
+    private const string GeneralKey = "General.Config";
+    private const string SlaKey = "Sla.Config";
+
+    public async Task<GeneralSettingDto> GetGeneralAsync()
+    {
+        var setting = await appSettingRepository.GetByKeyAsync(GeneralKey);
+        return setting?.SettingValue is null
+            ? new GeneralSettingDto() // default kalau belum pernah disimpan
+            : JsonSerializer.Deserialize<GeneralSettingDto>(setting.SettingValue)!;
+    }
+
+    public async Task UpdateGeneralAsync(GeneralSettingDto dto, Guid updatedBy)
+    {
+        await appSettingRepository.UpsertAsync(new AppSetting
+        {
+            SettingKey = GeneralKey,
+            SettingValue = JsonSerializer.Serialize(dto),
+            IsEncrypted = false,
+            UpdatedBy = updatedBy
+        });
+    }
+
+    public async Task<SlaSettingDto> GetSlaAsync()
+    {
+        var setting = await appSettingRepository.GetByKeyAsync(SlaKey);
+        return setting?.SettingValue is null
+            ? new SlaSettingDto()
+            : JsonSerializer.Deserialize<SlaSettingDto>(setting.SettingValue)!;
+    }
+
+    public async Task UpdateSlaAsync(SlaSettingDto dto, Guid updatedBy)
+    {
+        await appSettingRepository.UpsertAsync(new AppSetting
+        {
+            SettingKey = SlaKey,
+            SettingValue = JsonSerializer.Serialize(dto),
+            IsEncrypted = false,
+            UpdatedBy = updatedBy
+        });
+    }
+}
