111e913e62357ef99a581c93786efffa74d30d67
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-07-30 10:08:15 +0700

    V2-Update UserRepo, add username, GetAllAsync, Addasync, and UpdateAsync

--- src/05.Infrastructure/Repositories/UserRepository.cs
+++ src/05.Infrastructure/Repositories/UserRepository.cs
@@ -8,12 +8,30 @@ namespace TicketManagement.Infrastructure.Repositories;
 
 public class UserRepository(ApplicationDbContext context) : IUserRepository
 {
-    public async Task<User?> GetByIdAsync(Guid id) =>
-        await context.Users.FindAsync(id);
+    public async Task<User?> GetByIdAsync(Guid id) => await context.Users.FindAsync(id);
 
     public async Task<User?> GetByEmailAsync(string email) =>
         await context.Users.FirstOrDefaultAsync(u => u.Email == email);
 
+    public async Task<User?> GetByUsernameAsync(string username) =>
+        await context.Users.FirstOrDefaultAsync(u => u.Username == username);
+
     public async Task<bool> ExistsWithRoleAsync(Guid userId, UserRole role) =>
         await context.Users.AnyAsync(u => u.Id == userId && u.Role == role);
+
+    public async Task<IEnumerable<User>> GetAllAsync() =>
+        await context.Users.AsNoTracking().ToListAsync();
+
+    public async Task<User> AddAsync(User user)
+    {
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task UpdateAsync(User user)
+    {
+        context.Users.Update(user);
+        await context.SaveChangesAsync();
+    }
 }
