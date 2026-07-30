4c224e8c356e82ae6645fa434440c560814fa556
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-07-30 10:02:26 +0700

    V2-Add new interface for UserService

---
+++ src/04.Application/Interfaces/IProfileService.cs
@@ -0,0 +1,25 @@
+﻿using TicketManagement.Shared.Dtos.Profile;
+
+namespace TicketManagement.Application.Interfaces;
+
+public interface IProfileService
+{
+    Task<UserResponseDtoForProfile> GetAsync(Guid userId);
+    Task UpdateAsync(Guid userId, UpdateProfileDto dto);
+    Task ChangePasswordAsync(Guid userId, ChangePasswordDto dto);
+    Task<IEnumerable<ActivityLogDto>> GetActivityLogAsync(Guid userId);
+}
+
+// DTO kecil khusus tampilan profile diri sendiri
+public class UserResponseDtoForProfile
+{
+    public required string Username { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? JobTitle { get; set; }
+    public string? Address { get; set; }
+    public string? AvatarUrl { get; set; }
+    public required string Role { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
---
+++ src/04.Application/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using TicketManagement.Shared.Dtos.Users;
+
+namespace TicketManagement.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserResponseDto>> GetAllAsync();
+        Task<UserResponseDto> CreateAsync(CreateUserDto dto);
+        Task<UserResponseDto> UpdateAsync(Guid id, UpdateUserDto dto);
+        Task<UserResponseDto> ToggleStatusAsync(Guid id);
+    }
+}
