061e4eaca8dec498da04a7083f18b7a3e148c7ae
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-07-30 13:52:11 +0700

    V2-new Migration Add properties Timestamp in TicketHistories Entity

--- src/05.Infrastructure/Migrations/20260730064637_AddTimestamp_TicketHistory.Designer.cs
+++ src/05.Infrastructure/Migrations/20260730064637_AddTimestamp_TicketHistory.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TicketManagement.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using TicketManagement.Infrastructure.Persistence;
 namespace TicketManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260730064637_AddTimestamp_TicketHistory")]
+    partial class AddTimestamp_TicketHistory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
