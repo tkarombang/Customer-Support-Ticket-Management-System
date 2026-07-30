e347dc577d473158d7b1ebcfa67cf9489f38c870
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-07-30 21:08:42 +0700

    V2-Add DtoS for settings

---
+++ src/03.Shared/Dtos/Settings/GeneralSettingDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketManagement.Shared.Dtos.Settings
+{
+    public class GeneralSettingDto
+    {
+        public string AppName { get; set; } = "Ticket System";
+        public string? AppDescription { get; set; }
+        public string TimeZone { get; set; } = "Asia/Jakarta";
+        public string DateFormat { get; set; } = "dd MMM yyyy";
+        public string TimeFormat { get; set; } = "24h";
+        public string Language { get; set; } = "id";
+        public string Currency { get; set; } = "IDR";
+        public int ItemsPerPage { get; set; } = 10;
+    }
+}
---
+++ src/03.Shared/Dtos/Settings/SlaSettingDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketManagement.Shared.Dtos.Settings
+{
+    public class SlaSettingDto
+    {
+        public int HighPriorityHours { get; set; } = 4;
+        public int MediumPriorityHours { get; set; } = 24;
+        public int LowPriorityHours { get; set; } = 72;
+    }
+}
