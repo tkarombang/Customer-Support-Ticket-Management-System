4217e581bc675858840da0d6ae26475812b483f8
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-07-31 09:57:55 +0700

    V2-Add Interface and Dto for Backup Histories

---
+++ src/02.Domain/Interfaces/IBackupHistoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketManagement.Domain.Entities;
+
+namespace TicketManagement.Domain.Interfaces
+{
+    public interface IBackupHistoryRepository
+    {
+        Task AddAsync(BackupHistory history);
+        Task<IEnumerable<BackupHistory>> GetAllAsync();
+    }
+}
---
+++ src/03.Shared/Dtos/Settings/BackupHistoryResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketManagement.Shared.Dtos.Settings
+{
+    public class BackupHistoryResponseDto
+    {
+        public Guid BackupId { get; set; }
+        public required string FileName { get; set; }
+        public long? FileSizeBytes { get; set; }
+        public required string Type { get; set; }
+        public required string Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? TriggeredByName { get; set; }
+    }
+}
