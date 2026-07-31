e366366d6d36b7dc4161c73eda8ea658d7081984
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-07-31 10:00:03 +0700

    V2-New Dto SystemLog (filter and Item)

---
+++ src/03.Shared/Dtos/SystemLogs/SystemLogFilterDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TicketManagement.Shared.Dtos.SystemLogs
+{
+    public class SystemLogFilterDto
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string? Action { get; set; }
+        public Guid? UserId { get; set; }
+        public string? SearchTerm { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ src/03.Shared/Dtos/SystemLogs/SystemLogItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TicketManagement.Shared.Dtos.SystemLogs
+{
+    public class SystemLogItemDto
+    {
+        public Guid LogId { get; set; }
+        public string? UserName { get; set; }
+        public required string Action { get; set; }
+        public required string Description { get; set; }
+        public string? IpAddress { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
