d668939812660ee1a7021adc3498bbb806d487d2
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-07-31 15:51:28 +0700

    V2-add CRUD for Integration Config

---
+++ src/05.Infrastructure/Repositories/IntegrationConfigRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketManagement.Domain.Entities;
+using TicketManagement.Domain.Interfaces;
+using TicketManagement.Infrastructure.Persistence;
+
+namespace TicketManagement.Infrastructure.Repositories;
+
+public class IntegrationConfigRepository(ApplicationDbContext context) : IIntegrationConfigRepository
+{
+    public async Task<IEnumerable<IntegrationConfig>> GetAllAsync() =>
+        await context.IntegrationConfigs.AsNoTracking().ToListAsync();
+
+    public async Task<IntegrationConfig?> GetByIdAsync(Guid id) =>
+        await context.IntegrationConfigs.FindAsync(id);
+
+    public async Task<IntegrationConfig> AddAsync(IntegrationConfig config)
+    {
+        context.IntegrationConfigs.Add(config);
+        await context.SaveChangesAsync();
+        return config;
+    }
+
+    public async Task UpdateAsync(IntegrationConfig config)
+    {
+        context.IntegrationConfigs.Update(config);
+        await context.SaveChangesAsync();
+    }
+}
