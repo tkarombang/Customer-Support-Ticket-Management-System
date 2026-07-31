926dea5c07e0003e509f52936f02bb9cf68ac8fd
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-07-31 15:52:06 +0700

    V2-Add Controller filter for Ticket Histories

---
+++ src/06.WebApi/Controllers/TicketHistoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TicketManagement.Application.Interfaces;
+using TicketManagement.Shared.Constants;
+using TicketManagement.Shared.Dtos.TicketHistories;
+
+namespace TicketManagement.WebApi.Controllers;
+
+[ApiController]
+[Route(ApiRoutes.TicketHistories.Base)]
+[Authorize(Roles = RoleConstants.Administrator)]
+public class TicketHistoriesController(ITicketHistoryService historyService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetFiltered([FromQuery] TicketHistoryFilterDto filter)
+    {
+        var result = await historyService.GetFilteredAsync(filter);
+        return Ok(result);
+    }
+}
