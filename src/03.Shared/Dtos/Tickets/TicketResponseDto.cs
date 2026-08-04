f4feefea7886e6b9975bc05547defe34c2f1d1cb
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-08-04 13:03:46 +0700

    V2-Add new Properties in UpdateTicketDto for Populate Update Ticket

--- src/03.Shared/Dtos/Tickets/TicketResponseDto.cs
+++ src/03.Shared/Dtos/Tickets/TicketResponseDto.cs
@@ -4,6 +4,12 @@ public class TicketResponseDto
 {
     public Guid TicketId { get; set; }
     public required string TicketNumber { get; set; }
+    public required string Type { get; set; }
+    public required string Impact { get; set; }
+    public required string Category { get; set; }
+    public string? ApplicationSystem { get; set; }
+    public required string Priority { get; set; }
+    public DateTime? DueDate { get; set; }
     public required string CustomerName { get; set; }
     public required string CustomerEmail { get; set; }
     public required string Title { get; set; }
@@ -13,4 +19,6 @@ public class TicketResponseDto
     public string? AssignedToAgentName { get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime? UpdatedDate { get; set; }
+    public List<TicketAttachmentResponseDto> Attachments { get; set; } = [];
+    public List<TicketCommentResponseDto> Comments { get; set; } = [];
 }
--- src/03.Shared/Dtos/Tickets/UpdateTicketDto.cs
+++ src/03.Shared/Dtos/Tickets/UpdateTicketDto.cs
@@ -4,9 +4,15 @@ namespace TicketManagement.Shared.Dtos.Tickets;
 
 public class UpdateTicketDto
 {
-    [Required]
-    public required string Description { get; set; }
+    public required string Type { get; set; }
+    public required string Impact { get; set; }
+    public required string Category { get; set; }
+    public string? ApplicationSystem { get; set; }
+    public required string Priority { get; set; }
+    public DateTime? DueDate { get; set; }
 
-    [Required]
+    public required string Title { get; set; }
+    public required string Description { get; set; }
     public required string Status { get; set; } // "Open", "In Progress", "Resolved", "Closed"
+    public string? StatusNote { get; set; } // catatan opsional saat ganti status (jadi TicketComment
 }
