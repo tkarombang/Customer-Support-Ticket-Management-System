034d8c8979eddcfef7c79cf8c5be6818cd5b919d
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-08-06 10:12:23 +0700

    V2-New DTO for SLA Compliance

---
+++ src/03.Shared/Dtos/Reports/SlaComplianceDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketManagement.Shared.Dtos.Reports
+{
+    public class SlaComplianceDto
+    {
+        public double CompliancePercentage { get; set; }
+        public int TotalResolved { get; set; }
+        public int WithinSla { get; set; }
+        public int BreachedSla { get; set; }
+        public List<SlaComplianceTrendPointDto> Trend { get; set; } = [];
+    }
+}
