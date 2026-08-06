76fc9a9cfd48ddc2c58c0a76d37d53924adbef05
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-08-06 13:21:26 +0700

    V2-Update controller add GetSlaCompliannceAsync

--- src/06.WebApi/Controllers/ReportsController.cs
+++ src/06.WebApi/Controllers/ReportsController.cs
@@ -19,4 +19,18 @@ public class ReportsController(IReportService reportService) : ControllerBase
         var result = await reportService.GetManagerReportAsync(filter);
         return Ok(result);
     }
+
+    [HttpGet(ApiRoutes.Reports.SlaCompliance)]
+    public async Task<IActionResult> GetSlaCompliannceAsync(DateTime? startDate, DateTime? endDate)
+    {
+        var result = await reportService.GetSlaCompliannceAsync(startDate, endDate);
+        return Ok(result);
+    }
+
+    [HttpGet(ApiRoutes.Reports.ResponseTime)]
+    public async Task<IActionResult> GetResponseTime(DateTime? startDate, DateTime? endDate)
+    {
+        var result = await reportService.GetResponseTimeAsync(startDate, endDate);
+        return Ok(result);
+    }
 }
