a714e5d895bd3a250778678ae519ae2fca79b087
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-08-07 23:40:53 +0700

    V2-Add renderStatusChart function

--- src/08.Bsui/wwwroot/js/dashboard.js
+++ src/08.Bsui/wwwroot/js/dashboard.js
@@ -1,4 +1,9 @@
-﻿function loadDashboard() {
+﻿
+$(document).ready(function () {
+    loadDashboard();
+});
+
+function loadDashboard() {
     Swal.fire({
         title: 'Memuat Data...',
         html: 'Mohon tunggu sebentar.',
@@ -14,6 +19,7 @@
         success: function (summary) {
             Swal.close();
             renderSummary(summary);
+            renderStatusChart(summary)
         },
         error: function (xhr) {
             Swal.close();
@@ -38,19 +44,27 @@ function renderSummary(summary) {
     $('#resolvedCount').text(summary.resolvedCount);
     $('#closedCount').text(summary.closedCount);
 
-    const tbody = $('#workloadTableBody');
-    tbody.empty();
+    //const tbody = $('#workloadTableBody');
+    //tbody.empty();
 
-    if (!summary.workloadPerAgent || summary.workloadPerAgent.length === 0) {
-        tbody.append('<tr><td colspan="2" class="text-center">Belum ada tiket yang ditugaskan</td></tr>');
-        return;
-    }
+    //if (!summary.workloadPerAgent || summary.workloadPerAgent.length === 0) {
+    //    tbody.append('<tr><td colspan="2" class="text-center">Belum ada tiket yang ditugaskan</td></tr>');
+    //    return;
+    //}
 
-    summary.workloadPerAgent.forEach(function (w) {
-        tbody.append(`<tr><td>${w.agentName}</td><td>${w.assignedTicketCount}</td></tr>`);
-    });
+    //summary.workloadPerAgent.forEach(function (w) {
+    //    tbody.append(`<tr><td>${w.agentName}</td><td>${w.assignedTicketCount}</td></tr>`);
+    //});
 }
 
-$(document).ready(function () {
-    loadDashboard();
-});
+function renderStatusChart(summary) {
+     new Chart($('#chartDashStatus'), {
+        type: 'doughnut',
+        data: {
+            labels: ['Open', 'In Progress', 'Resolved', 'Closed'],
+            datasets: [{ data: [summary.openCount, summary.inProgressCount, summary.resolvedCount, summary.closedCount],
+                backgroundColor: ['#2563eb', '#f59e0b', '#16a34a', '#64748b'] }]
+        },
+        options: { responsive: true, maintainAspectRatio: false, plugins: { legend: { position: 'bottom' } } }
+    });
+}
