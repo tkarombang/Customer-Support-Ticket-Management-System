1aee99d5587994c01013d4bcf3a6e18ee6e044a4
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-08-09 11:28:13 +0700

    V2-Calling Ajax for General Tabs Settings

---
+++ src/08.Bsui/wwwroot/js/settings.js
@@ -0,0 +1,30 @@
+﻿function getAntiForgeryToken() {
+    return $('input[name="__RequestVerificationToken"]').val()
+}
+
+$(document).ready(function () {
+    switchTab('general')
+})
+
+function switchTab(tabName) {
+    loadTabData(tabName)
+}
+
+function loadTabData(tabName) {
+    if (tabName === 'general') loadGeneral()
+}
+
+function loadGeneral() {
+    $.ajax({
+        url: '/Settings?handler=General',
+        method: 'GET',
+        success: function (g) {
+            $('#gen-appName').val(g.appName);
+            $('#gen-appDesc').val(g.appDescription);
+            $('#gen-timezone').val(g.timeZone);
+            $('#gen-language').val(g.language);
+            $('#gen-dateFormat').val(g.dateFormat);
+            $('#gen-itemsPerPage').val(g.itemsPerPage);
+        }
+    });
+}
