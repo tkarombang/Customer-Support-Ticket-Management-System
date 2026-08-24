e69d10c218793d8d62c4b560e6515841bb3954a0
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-08-24 10:47:18 +0700

    V2-create new file for interface SystemLogService

---
+++ src/04.Application/Interfaces/ISystemLogService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketManagement.Domain.Enums;
+
+namespace TicketManagement.Application.Interfaces
+{
+    public interface ISystemLogService
+    {
+        Task LogAsync(Guid? userId, SystemLogAction action, string description, string? ipAddress = null);
+    }
+}
--- src/04.Application/Services/SystemLogService.cs
+++ src/04.Application/Services/SystemLogService.cs
@@ -1,15 +1,9 @@
-﻿using System.Security.Cryptography;
+﻿using TicketManagement.Application.Interfaces;
 using TicketManagement.Domain.Entities;
 using TicketManagement.Domain.Enums;
 using TicketManagement.Domain.Interfaces;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace TicketManagement.Application.Services;
-
-public interface ISystemLogService
-{
-    Task LogAsync(Guid? userId, SystemLogAction action, string description, string? ipAddress = null);
-}
 
 public class SystemLogService(ISystemLogRepository systemLogRepository) : ISystemLogService
 {
