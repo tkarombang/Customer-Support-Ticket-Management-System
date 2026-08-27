03468591b0c959b4b4ef64c8c92379d45a0b6e31
Muhammad Azwar Anas <tuangKarombang@gmail.com>
2026-08-27 16:51:28 +0700

    V2-Fix Bug: DateTime.Kind hilang  setelah Roundtrip  ke database

---
+++ src/06.WebApi/Converters/UtcDateTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TicketManagement.WebApi.Converters;
+public class UtcDateTimeConverter : JsonConverter<DateTime>
+{
+    public override DateTime Read(
+        ref Utf8JsonReader reader, 
+        Type typeToConvert, 
+        JsonSerializerOptions options
+        ) => reader.GetDateTime();
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        DateTime value,
+        JsonSerializerOptions options)
+    {
+        var utcValue = value.Kind == DateTimeKind.Unspecified
+            ? DateTime.SpecifyKind(value, DateTimeKind.Utc)
+            : value.ToUniversalTime();
+
+        writer.WriteStringValue(utcValue);
+    }
+}
--- src/06.WebApi/Program.cs
+++ src/06.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using TicketManagement.Infrastructure;
+using TicketManagement.WebApi.Converters;
 using TicketManagement.WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,11 @@ builder.Services.AddControllers();
 builder.Services.AddSwaggerDocumentation();
 builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new UtcDateTimeConverter());
+    });
 
 var app = builder.Build();
 
